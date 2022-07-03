31843c9be3288956d0f3574f32903a57ff0c026a
phitchayakorn <phitchayakorn.cpe@outlook.com>
2022-07-03 23:14:36 +0700

    Question 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'section2-2';
+  results: any = null;
+  dataArr: any = null;
+  filterStr = '';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getApi();
+  }
+
+  getApi() {
+    this.http
+      .get('https://api.publicapis.org/categories ')
+      .subscribe((data: any) => {
+        this.results = data.categories;
+
+        this.searchCategories();
+      });
+  }
+
+  searchCategories() {
+    this.dataArr = this.results;
+    if (this.filterStr) {
+      this.dataArr = this.dataArr.filter((val: any) => {
+        return val.includes(this.filterStr);
+      });
+    }
+  }
 }
